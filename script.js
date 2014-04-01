4bff7a7dc099d0a0d0f5a1718fb6635d8822dd1b
Brian Jones <bklynjones@mac.com>
2014-04-01 10:50:12 -0400

    added var for ip

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-
+				var ip = 'http://172.26.14.7:8080'
 				var entered_devicename,
 					entered_devicenamewrite,
 					entered_value;
@@ -7,7 +7,7 @@ window.onload = function(){
     			$("#getrecordbydevice").click(function(){
 
     					entered_devicename = $('#devicenameval').val();
-    					var url = 'http://localhost:10080/read-latest?devicename=' + entered_devicename;
+    					var url = ip +'/read-latest?devicename=' + entered_devicename;
     					console.log('the name is' + entered_devicename);
     					$.getJSON(url, function(result){
     						$.each(result, function(i,obj){
@@ -23,7 +23,7 @@ window.onload = function(){
 
     					entered_devicenamewrite = $('#devicenamevalwrite').val();
     					entered_value = $('#writevalue').val();
-    					var url = 'http://localhost:10080/write?devicename='+entered_devicenamewrite+ '&val1=' + entered_value;
+    					var url = ip + '/write?devicename='+entered_devicenamewrite+ '&val1=' + entered_value;
     					$.get(url,function(data){
     					//callback goes here on success
     					});
