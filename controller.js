635a6faa97f8371e6ae5e14f994baa2e6040b767
Brian Jones <bklynjones@mac.com>
2014-04-01 02:42:16 -0400

    ignore

--- controller.js
+++ controller.js
@@ -1,4 +1,25 @@
-angular.module('hello', [])
+angular.module('hello', []) //.module takes two parameters the first is it's name, the second is a list of dependencies
 	.controller('HelloCtrl', function($scope){
 		$scope.name = 'World';
+
+		$scope.resp = $http({method: 'GET', url: 'http://localhost:10080/read-latest',
+			params:{
+				devicename=dribblea
+			}
+		}).success(function(data, status, headers, config) {
+		
+
+		});
+
+
 	});
+
+angular.module('read',[])
+	.controller('NDBCtrl', function($scope){
+
+		$scope.name = 'foo';
+
+		
+};
+
+
